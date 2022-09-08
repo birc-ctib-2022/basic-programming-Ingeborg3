b132cd5083a4b87eb309882a97d83247c057cb2e
Ingeborg3 <111577212+Ingeborg3@users.noreply.github.com>
2022-09-08 18:47:50 +0200

    First 4 exercises

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,16 @@
 
 # Print the pattern
+
+for ln in range(2, 7):
+    for n in range(1, ln):
+        if n == ln-1:
+            print('*')
+        else:
+            print('*', end=' ')
+
+for ln in range(5, 0, -1):
+    for n in range(1, ln): 
+        if n==ln-1:
+            print('*')
+        else:
+            print('*', end=' ')
