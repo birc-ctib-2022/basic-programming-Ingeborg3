1c23d8da3f1f4577f66bb48de9041905205bdc56
Ingeborg3 <111577212+Ingeborg3@users.noreply.github.com>
2022-09-08 19:54:24 +0200

    Password and growind exercise

--- src/password.py
+++ src/password.py
@@ -6,7 +6,29 @@ if len(sys.argv) != 2:
 
 password = sys.argv[1]
 is_valid = False
-
 # Do all the requirement checks here.
 
+cond1, cond2, cond3, cond4, cond5, cond6 = False, False, False, False, False, False
+for c in password:
+    if c.isupper(): # Only returns True if all letters in the 
+        # argument string are uppercase. Non-letters are ignored.
+        cond1 = True # Condition true if at least one of the characters
+        # are uppercase. 
+for c in password:
+    if c.islower():
+        cond2 = True
+for c in password:
+    if c.isnumeric():
+        cond3 = True
+for c in password:
+    if c in '$#@':
+        cond4 = True
+if len(password) >= 6:
+    cond5 = True
+if len(password) <= 16:
+    cond6 = True
+
+if cond1 == True & cond2 == True & cond3 == True & cond4 == True & cond5 == \
+    True & cond6 == True:
+    is_valid = True
 print(is_valid)
